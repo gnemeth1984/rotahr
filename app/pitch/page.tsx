d83456fdd62135e907416b12f7b2e089a1976468
Gabor Nemeth <gabor@rotahr.app>
2026-06-26 09:14:54 +0000

    feat: replace CTA slide with Benefits + CTA combined closing slide

--- app/pitch/page.tsx
+++ app/pitch/page.tsx
@@ -14,7 +14,7 @@ export default function PitchPage() {
     "https://storage.googleapis.com/runable-templates/cli-uploads%2FnpR7iwoxdmjjem91IBsyOE3SjrOrbdCS%2FeLaHSlkaiQWt3Cfra0DtX%2Fslide-08.png",
     "https://storage.googleapis.com/runable-templates/cli-uploads%2FnpR7iwoxdmjjem91IBsyOE3SjrOrbdCS%2FeQC1bTyeQBqJtYaXhaVEp%2Fslide-09.png",
     "https://storage.googleapis.com/runable-templates/cli-uploads%2FnpR7iwoxdmjjem91IBsyOE3SjrOrbdCS%2FeCMVe4vHAQ-PrsknvOquJ%2Fslide-10.png",
-    "https://storage.googleapis.com/runable-templates/cli-uploads%2FnpR7iwoxdmjjem91IBsyOE3SjrOrbdCS%2FdA81Ex6x0ncIMIm1rYdqF%2Fslide-12.png",
+    "https://storage.googleapis.com/runable-templates/cli-uploads%2FnpR7iwoxdmjjem91IBsyOE3SjrOrbdCS%2Fl7DSqDT0H65hum-LPN2xe%2Fslide-11-new.png",
   ];
 
   useEffect(() => {
